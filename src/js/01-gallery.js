bb5be9c7d4a2837ed224308b8b29739ee3e58e6b
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-21 00:07:46 +0200

    Виконано завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,19 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryListUlEl = document.querySelector('.gallery');
 
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+};
+
+const galleryCardsAll = galleryItems.map(el => makeGalleryCard(el)).join('');
+galleryListUlEl.insertAdjacentHTML('beforeend', galleryCardsAll);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
